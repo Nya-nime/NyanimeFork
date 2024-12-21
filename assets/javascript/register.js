54c14d0cfa8e28c595f005f38d9b987e55f4bcbf
Haekalsss <kallsss145@gmail.com>
2024-12-22 00:48:20 +0700

    test

--- assets/javascript/register.js
+++ assets/javascript/register.js
@@ -9,30 +9,30 @@ registerForm.addEventListener('submit', (event) => {
     const password = document.getElementById('password').value;
     const role = document.getElementById('role').value;
 
-    fetch('http://localhost:8080/user/register', { // Pastikan URL ini sesuai dengan backend Anda
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ username, email, password, role }),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Registration failed');
-        }
-        return response.json();
-    })
-    .then(data => {
-        alert('Registration successful! Welcome, ' + data.user.username);
-        // Redirect based on role
-        if (data.user.role === 'admin') {
-            window.location.href = 'admin_home.html'; // Redirect to admin home page
-        } else {
-            window.location.href = 'user_home.html'; // Redirect to user home page
-        }
-    })
-    .catch(error => {
-        console.error('There was a problem with the fetch operation:', error);
-        alert('Registration failed: ' + error.message);
-    });
+    fetch('http://localhost:8080/user/register', {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, email, password, role }),
+  })
+  .then(response => {
+      if (!response.ok) {
+          throw new Error('Registration failed');
+      }
+      return response.json();
+  })
+  .then(data => {
+      alert('Registration successful! Welcome, ' + data.user.username);
+      // Redirect based on role
+      if (data.user.role === 'admin') {
+          window.location.href = 'admin_home.html'; // Redirect to admin home page
+      } else {
+          window.location.href = 'user_home.html'; // Redirect to user home page
+      }
+  })
+  .catch(error => {
+      console.error('There was a problem with the fetch operation:', error);
+      alert('Registration failed: ' + error.message);
+  });
 });
